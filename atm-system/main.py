e5d0ce4a23bfe69a4bad32986bce2135cd33a9f6
Raghav Krishna <raghavgamerz670@gmail.com>
2026-04-14 15:38:19 +0530

    Restructure project into atm-system folder and rename file to main.py

---
+++ atm-system/main.py
@@ -0,0 +1,36 @@
+acc_balance = 1000
+
+while True:
+
+    print("1. Withdraw")
+    print("2. Check Balance")
+    print("3. Exit")
+
+    choice = input("Enter option: ")
+
+    if choice == "1":
+        print("Hi, how much money you want to withdraw?")
+        amt_to_withdraw = int(input("Enter amount to withdraw: "))
+
+        if amt_to_withdraw <= 0:
+            print("Invalid amount.")
+
+        elif amt_to_withdraw > acc_balance:
+            print("Insufficient balance in the account")
+            print("You only have " + str(acc_balance))
+
+        else:
+            acc_balance = acc_balance - amt_to_withdraw
+            print("Withdrawal successful!")
+            print("You have withdrawn rupees " + str(amt_to_withdraw))
+            print("You have " + str(acc_balance) + " remaining in your account.")
+
+    elif choice == "2":
+        print("Your Balance is " + str(acc_balance))
+
+    elif choice == "3":
+        print("Thank you for using ATM!")
+        break
+
+    else:
+        print("Invalid option. Try again.")
