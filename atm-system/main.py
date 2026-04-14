121ea754ffde7c16086dfc3cd3d225c5b2790eb7
Raghav Krishna <raghavgamerz670@gmail.com>
2026-04-14 15:45:07 +0530

    Add PIN authentication before ATM menu

--- atm-system/main.py
+++ atm-system/main.py
@@ -1,36 +1,38 @@
 acc_balance = 1000
+correct_pin = 3075
+entered_pin = int(input("Enter the PIN: "))
 
-while True:
+if correct_pin == entered_pin:
+    while True:
+        print("1. Withdraw")
+        print("2. Check Balance")
+        print("3. Exit")
+        choice = input("Enter option: ")
+        if choice == "1":
+            print("Hi, how much money you want to withdraw?")
+            amt_to_withdraw = int(input("Enter amount to withdraw: "))
 
-    print("1. Withdraw")
-    print("2. Check Balance")
-    print("3. Exit")
+            if amt_to_withdraw <= 0:
+                print("Invalid amount.")
 
-    choice = input("Enter option: ")
+            elif amt_to_withdraw > acc_balance:
+                print("Insufficient balance in the account")
+                print("You only have " + str(acc_balance))
 
-    if choice == "1":
-        print("Hi, how much money you want to withdraw?")
-        amt_to_withdraw = int(input("Enter amount to withdraw: "))
+            else:
+                acc_balance = acc_balance - amt_to_withdraw
+                print("Withdrawal successful!")
+                print("You have withdrawn rupees " + str(amt_to_withdraw))
+                print("You have " + str(acc_balance) + " remaining in your account.")
 
-        if amt_to_withdraw <= 0:
-            print("Invalid amount.")
+        elif choice == "2":
+            print("Your Balance is " + str(acc_balance))
 
-        elif amt_to_withdraw > acc_balance:
-            print("Insufficient balance in the account")
-            print("You only have " + str(acc_balance))
+        elif choice == "3":
+            print("Thank you for using ATM!")
+            break
 
         else:
-            acc_balance = acc_balance - amt_to_withdraw
-            print("Withdrawal successful!")
-            print("You have withdrawn rupees " + str(amt_to_withdraw))
-            print("You have " + str(acc_balance) + " remaining in your account.")
-
-    elif choice == "2":
-        print("Your Balance is " + str(acc_balance))
-
-    elif choice == "3":
-        print("Thank you for using ATM!")
-        break
-
-    else:
-        print("Invalid option. Try again.")
+            print("Invalid option. Try again.")
+else:
+    print("Wrong PIN, Goodbye!")
