c67b285af472f4b7d68cfe5bb3b7e68544758659
Raghav Krishna <raghavgamerz670@gmail.com>
2026-04-15 18:06:36 +0530

    Add transaction history feature

--- atm-system/main.py
+++ atm-system/main.py
@@ -2,18 +2,18 @@ acc_balance = 1000
 correct_pin = 3075
 max_attempts = 3
 attempts_left = max_attempts
+transactions = []
 
-# PIN verification loop
 while attempts_left > 0:
     entered_pin = int(input("Enter the PIN: "))
     
     if correct_pin == entered_pin:
-        # PIN correct – now enter ATM menu loop
         while True:
             print("\n1. Withdraw")
             print("2. Deposit")
             print("3. Check Balance")
-            print("4. Exit")
+            print("4. View Transactions")
+            print("5. Exit")
             choice = input("Enter option: ")
             
             if choice == "1":
@@ -30,6 +30,7 @@ while attempts_left > 0:
                     print("Withdrawal successful!")
                     print("You have withdrawn rupees " + str(amt_to_withdraw))
                     print("You have " + str(acc_balance) + " remaining in your account.")
+                    transactions.append("Withdraw: " + str(amt_to_withdraw))
             
             elif choice == "2":
                 print("How much money you want to deposit?")
@@ -41,18 +42,24 @@ while attempts_left > 0:
                     acc_balance = acc_balance + amt_to_deposit
                     print("You have deposited " + str(amt_to_deposit))
                     print("Your new balance is: " + str(acc_balance))
+                    transactions.append("Deposit: " + str(amt_to_deposit))
             
             elif choice == "3":
                 print("Your Balance is " + str(acc_balance))
             
             elif choice == "4":
+                print("Transaction History:")
+                for transaction in transactions:
+                    print(transaction)
+            
+            elif choice == "5":
                 print("Thank you for using ATM!")
-                break   # exits the ATM menu loop
+                break   
             
             else:
                 print("Invalid option. Try again.")
         
-        break   # exits the PIN verification loop (important!)
+        break   
     
     else:
         attempts_left = attempts_left - 1
